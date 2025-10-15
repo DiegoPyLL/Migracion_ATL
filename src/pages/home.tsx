56d5bdb067bbf8ef6e465671d65e7591010bb2eb
Diego <dpenaylilloluhrs@gmail.com>
2025-10-15 18:40:22 -0300

    Base v1.4

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
 
-  /* cambio pa ver si el wn de isac se salio */
+
   return (
     <>
       <div>
